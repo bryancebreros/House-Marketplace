788322635495d1a22894c1b329f4863d646d0558
Bryan Sandoval <cebrerosbryan@gmail.com>
2022-08-04 13:54:57 -0600

    Enables house listing

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import 'react-toastify/dist/ReactToastify.css'
 import PrivateRoute from './components/PrivateRoute';
 import Explore from "./pages/Explore";
 import Offers from "./pages/Offers"; 
+import Category from './pages/Category'
 import Profile from "./pages/Profile";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 import ForgotPass from "./pages/ForgotPass";
 import Navbar from './components/Navbar';
+import CreateListing from './pages/CreateListing';
 function App() {
   return (
     <>
@@ -16,6 +18,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Explore />} />
         <Route path='/offers' element={<Offers />} />
+        <Route path='/category/:categoryName' element={<Category />} />
         {/* redirects to signIn if theres no user */}
         <Route path='/profile' element={<PrivateRoute />}>
           <Route path='/profile' element={<Profile />}/>
@@ -23,6 +26,7 @@ function App() {
         <Route path='/signin' element={<Signin />} />
         <Route path='/signup' element={<Signup />} />
         <Route path='/forgotpass' element={<ForgotPass />} />
+        <Route path='/create-listing' element={<CreateListing />} />
       </Routes>
       <Navbar />
     </Router>
--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -15,7 +15,7 @@ import {toast} from 'react-toastify'
 import Spinner from '../components/Spinner'
 import ListingItem from '../components/ListingItem'
 
-function Offers() {
+function Category() {
     const [listings, setListings] = useState(null)
     const [loading, setLoading] = useState(true)
 
@@ -26,7 +26,7 @@ function Offers() {
             try {
                 const listingsRef = collection(db, 'listings')
 
-                const q = query(listingsRef, where('offer', '==', true), orderBy('timestamp', 'desc'), limit(10))
+                const q = query(listingsRef, where('type', '==', params.categoryName), orderBy('timestamp', 'desc'), limit(10))
                 
                 const querySnap = await getDocs(q)
                 
@@ -46,12 +46,14 @@ function Offers() {
             }
         }
         fetchListings()
-    }, [])
+    }, [params.categoryName])
   return (
     <div className='category'>
         <header>
             <p className="pageHeader">
-                Offers
+                {params.categoryName === 'rent'
+                    ? 'Places for rent'
+                    : 'Places for sale'}
             </p>
         </header>
         {loading ? (<Spinner />) : listings && listings.length > 0 ?( 
@@ -71,4 +73,4 @@ function Offers() {
   )
 }
 
-export default Offers
+export default Category
